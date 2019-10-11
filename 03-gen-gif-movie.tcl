caef3606ec797e865af81cdceb52886f694725b8
qi-wang-uc <wangqi1900@gmail.com>
2019-10-11 18:02:07 -0400

    update comment

--- 03-gen-gif-movie.tcl
+++ 03-gen-gif-movie.tcl
@@ -1,7 +1,7 @@
 # Source this script from VMD TkConsole to generate GIFs from MD trajectory.
 # Parameters:
 #  moviename - output name (without the .gif extension) for the GIF file
-#  frameskip - number of frames to be skipped (default is 1)
+#  frameskip - number of frames to be skipped (optional, default is 1)
 proc makemovie { moviename { frameskip 1}} {
     set nframes [molinfo top get numframes]
     for { set iframe 0 } { $iframe < $nframes } { incr iframe $frameskip} {
