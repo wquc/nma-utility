c6f2152d52d9b50a8748c82bf84d4672f4ba975b
qi-wang-uc <wangqi1900@gmail.com>
2019-10-11 17:49:58 -0400

    add option to skip frames

--- 03-gen-gif-movie.tcl
+++ 03-gen-gif-movie.tcl
@@ -1,15 +1,17 @@
 # Source this script from VMD TkConsole to generate GIFs from MD trajectory.
-
-proc make_animated_gif {} {
+# Parameters:
+#  moviename - output name (without the .gif extension) for the GIF file
+#  frameskip - number of frames to be skipped (default is 1)
+proc makemovie { moviename { frameskip 1}} {
     set nframes [molinfo top get numframes]
-    for { set iframe 0 } { $iframe < $nframes } { incr iframe 1} {
+    for { set iframe 0 } { $iframe < $nframes } { incr iframe $frameskip} {
         animate goto $iframe
-        set filename snap.[format "%04d" $iframe].rgb
-        render snapshot $filename
+        set filename tmp.[format "%05d" [expr $iframe+1]].ppm
+        render TachyonLOptiXInternal $filename
     }
     puts "Converting tmp files to gif ... \n"
-    exec convert -delay 10 -loop 0 snap.*.rgb movie.gif
+    exec convert -delay 15 -loop 0 tmp.*.ppm $moviename.gif
     puts "Deleting tmp files ... \n"
-    exec /bin/sh -c "rm snap.*.rgb"
+    exec /bin/sh -c "rm tmp.*.ppm"
     puts "Done. \n"
 }
